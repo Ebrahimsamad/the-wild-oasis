48a65f31e81561c01d54a742750b160dbcf36172
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-01-18 18:31:45 +0200

    update small

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 import Spinner from "../../ui/Spinner";
-import Stats from "./stats";
+import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
 import TodayActivity from "../check-in-out/TodayActivity";
